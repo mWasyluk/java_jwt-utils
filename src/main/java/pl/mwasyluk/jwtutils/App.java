fb3998cdecef49e6d670093c9db596184fe520b7
mWasyluk <marekwasyluk@wp.pl>
2023-11-24 21:05:44 +0100

    origin

---
+++ src/main/java/pl/mwasyluk/jwtutils/App.java
@@ -0,0 +1,13 @@
+package pl.mwasyluk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
